e713ae460c88b9264fe6ff8ea9d85c44be55618a
Nihal upreti <n.ytuchiha@gmail.com>
2023-12-24 01:50:42 +0545

    schedule path dummy data

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from . views import *
-
+from .views import *
 
 urlpatterns = [
-    path('', HomePage.as_view(), name='home'),
+    path('', HomePageView.as_view(), name='home'),
+    path('complain/', ComplainView.as_view(), name='complain'),
+    path('schedule/', ScheduleView.as_view(), name='schedule'),
+
 ]
--- home/views.py
+++ home/views.py
@@ -1,12 +1,49 @@
 from django.shortcuts import render
 from django.views.generic import View
+from .models import *
 
 
 class Base(View):
     views = {}
 
 
-class HomePage(Base):
+class HomePageView(Base):
     def get(self, request):
         return render(request, "home.html", self.views)
 
+
+class ComplainView(Base):
+
+    def get(self, request):
+        return render(request, "complaint.html", self.views)
+
+    def post(self, request):
+        if request.method == 'POST':
+            fullname = request.POST['fullname']
+            address = request.POST['address']
+            email = request.POST['email']
+            message = request.POST['message']
+            location = request.POST['location']
+            phone_number = request.POST['phone_number']
+
+            data = Feedback.objects.create(
+
+                fullname=fullname,
+                address=address,
+                email=email,
+                phone_number=phone_number,
+                message=message,
+                location=location,
+
+            )
+            data.save()
+
+        return render(request, "complaint.html", self.views)
+
+
+class ScheduleView(Base):
+
+    def get(self, request):
+        self.views['schedules'] = Schedule.objects.all
+
+        return render(request, "schedule.html", self.views)
