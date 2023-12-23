9f7ab5cd48f14fb9d0ae1d329534d0149e9ccb1d
Nihal upreti <n.ytuchiha@gmail.com>
2023-12-23 10:54:51 +0545

    removed home dir

--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Schedule(models.Model):
+    location = models.CharField(max_length=200)
+    date = models.DateField()
+    time = models.TimeField()
+
+    def __str__(self):
+        return self.location
+
